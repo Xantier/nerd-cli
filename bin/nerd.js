b35b1f9410055118ff8112014cae96d4d5ed72b3
Jussi Hallila <jussi@hallila.com>
2015-06-17 18:17:13 +0100

    Baby steps. Added initial question to CLI and folder creation based on that.

--- bin/nerd.js
+++ bin/nerd.js
@@ -1,5 +1,21 @@
 #!/usr/bin/env node
-
 'use strict';
 
-console.log('Hello, World!');
+var mkdirp = require('mkdirp');
+var prompt = require('prompt');
+
+prompt.start();
+
+prompt.get([{
+  name: 'project',
+  description: 'Name of your Application',
+  default: 'nerd-stack',
+  required: true
+}], function (err, result) {
+  console.log('Command-line input received:');
+  console.log('  username: ' + result.project);
+  mkdirp(process.cwd() + '/' + result.project, function (err) {
+    if(err) console.log(err);
+    console.log('Folder created');
+  });
+});
