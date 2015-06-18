9cbdfe440a1df16bb9161e14046eb8fa875d53a0
Jussi Hallila <jussi@hallila.com>
2015-06-18 19:03:26 +0100

    Added npm init package.json.

--- bin/nerd.js
+++ bin/nerd.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 'use strict';
 
+var path = require('path');
 var mkdirp = require('mkdirp');
 var prompt = require('prompt');
+var init = require('init-package-json');
+var initFile = path.resolve(process.env.HOME, '.npm-init');
 
 prompt.start();
 
@@ -16,6 +19,10 @@ prompt.get([{
   console.log('  username: ' + result.project);
   mkdirp(process.cwd() + '/' + result.project, function (err) {
     if(err) console.log(err);
-    console.log('Folder created');
+    init(process.cwd() + '/' + result.project, initFile, {}, function (err, data) {
+      if (err) console.log(err);
+      console.log(data);
+    })
   });
+
 });
