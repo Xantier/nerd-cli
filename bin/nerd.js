578180ccd944a21a02bee40fdd4ce71e3115266d
Jussi Hallila <jussi@hallila.com>
2015-06-22 20:37:10 +0100

    Added extra folder deletions and config file modifying.

--- bin/nerd.js
+++ bin/nerd.js
@@ -2,6 +2,8 @@
 'use strict';
 
 var path = require('path');
+var fs = require('fs');
+var rimraf = require('rimraf');
 var mkdirp = require('mkdirp');
 var prompt = require('prompt');
 var init = require('init-package-json');
@@ -19,25 +21,56 @@ var promptSchema = [{
   {
     name: 'database',
     description: 'What database do you want to use?',
-    default: 'rethinkDB',
+    default: 'rethinkdb',
     required: true
   }
 ];
 prompt.get(promptSchema, function (err, result) {
   console.log('Project name: ' + result.project);
-  console.log('Database name: ' + result.database);
+  console.log('Database type: ' + result.database);
   console.log('Let\'s initialize your package.json');
   mkdirp(process.cwd() + '/' + result.project, function (err) {
-    if(err) console.log(err);
-    ghDl({user: 'xantier', repo: 'nerd-stack', ref: 'master'}, process.cwd() + '/' + result.project)
-        .on('error', function(err) {
+    if (err) console.log(err);
+    var nerdPath = process.cwd() + '/' + result.project;
+    ghDl({user: 'xantier', repo: 'nerd-stack', ref: 'master'}, nerdPath)
+        .on('error', function (err) {
           console.error(err)
+        })
+        .on('dir', function (dir) {
+          console.log(dir);
+        })
+        .on('end', function () {
+          init(process.cwd() + '/' + result.project, initFile, {}, function (err, data) {
+            if (err) console.log(err);
+            console.log(data);
+          });
+          fs.readdir(nerdPath + '/app/data', function (err, list) {
+            for (var i in list) {
+              if (list.hasOwnProperty(i)) {
+                if (list[i] !== result.database) {
+                  rimraf(nerdPath + '/app/data/' + list[i], function (err) {
+                    if (err) console.log(err);
+                  });
+                }
+              }
+            }
+          });
+          var configPath = '/app/config/config.json';
+          var config = require(nerdPath + configPath);
+          fs.unlinkSync(nerdPath + configPath, 0, function (err) {
+            if (err) console.log(err);
+          });
+          var nuConfig = {};
+          nuConfig.db = result.database;
+          nuConfig[result.database] = config[result.database];
+          fs.writeFile(nerdPath + configPath, JSON.stringify(nuConfig, null, 4), function (err) {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log('config updated');
+            }
+          });
         });
-
-    init(process.cwd() + '/' + result.project, initFile, {}, function (err, data) {
-      if (err) console.log(err);
-      console.log(data);
-    })
   });
 
 });
